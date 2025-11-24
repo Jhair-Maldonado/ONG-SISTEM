7e1c729ec7f9e14cc0afdf014e0189f6d64288a0
Jhair-Maldonado <jhairmaldonadoquintana.2004@gmail.com>
2025-11-23 23:15:45 -0500

    Refactor api.ts by removing comments and updating methods
    Removed comments and updated the getAll and create methods in the API service.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,4 @@
-// Simula latencia de red
 const DELAY = 500;
-
-// Función auxiliar para manejar LocalStorage
 const getFromStorage = <T>(key: string, defaultData: T[]): T[] => {
   const stored = localStorage.getItem(key);
   if (stored) return JSON.parse(stored);
@@ -13,7 +10,6 @@ const saveToStorage = <T>(key: string, data: T[]) => {
 };
 
 export const api = {
-  // Ahora pide una 'key' (nombre de la tabla) para saber dónde buscar
   getAll: <T>(key: string, mockData: T[]): Promise<T[]> => {
     return new Promise((resolve) => {
       setTimeout(() => {
@@ -26,11 +22,8 @@ export const api = {
   create: <T>(key: string, newItem: T): Promise<T> => {
     return new Promise((resolve) => {
       setTimeout(() => {
-        // 1. Leemos lo que ya existe
         const currentData = getFromStorage<T>(key, []);
-        // 2. Agregamos lo nuevo
         const updatedData = [...currentData, newItem];
-        // 3. Guardamos en el "Disco Duro" (LocalStorage)
         saveToStorage(key, updatedData);
         resolve(newItem);
       }, DELAY);
