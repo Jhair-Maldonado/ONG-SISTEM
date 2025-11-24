a5987b906718ed4d19766e8c0470adaaf91c53b7
Jhair-Maldonado <jhairmaldonadoquintana.2004@gmail.com>
2025-11-23 23:17:24 -0500

    Refactor fontFamily configuration in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,13 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'], // Define Inter como la fuente sans por defecto
+        sans: ['Inter', 'sans-serif'],
       },
-      // Definimos las animaciones personalizadas
       animation: {
         'fadeIn': 'fadeIn 0.3s ease-out forwards',
         'slideUp': 'slideUp 0.4s ease-out forwards',
         'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
-      // Definimos los keyframes (los pasos de la animación)
       keyframes: {
         fadeIn: {
           '0%': { opacity: '0', transform: 'scale(0.95)' },
